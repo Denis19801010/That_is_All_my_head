f6fabab76e09232d57cdc7c5d4cc03ffe9fa8652
Денис-ПК <denisbataev351@gmail.com>
2022-04-29 14:58:54 +0300

    symen do

--- venv/CaesarCrypt.py
+++ venv/CaesarCrypt.py
@@ -2,18 +2,21 @@
 SYMBOLS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 MAX_KEY_SIZE = len(SYMBOLS)
 
+
 def getMode():
     while True:
         print('Вы хотите зашифровать или расшифровать текст?')
         mode = input().lower()
-        if mode in ['зашифровать', 'з', 'расшифровать', 'p']:
+        if mode in ['зашифровать', 'з', 'расшифровать', 'р']:
             return mode
         else:
             print('Введите"зашифровать"или"з"для зашифровки"или"расшифровать"или"р"для расшифровки.')
 
+
 def getMessage():
     print('Введите текст:')
     return input()
+
 
 def getKey():
     key = 0
@@ -23,14 +26,15 @@ def getKey():
         if (key >= 1 and key <= MAX_KEY_SIZE):
             return key
 
-def getTranslatedMessage(made, message, key):
-    if mode[0] =='р':
+
+def getTranslatedMessage(mode, message, key):
+    if mode[0] == 'р':
         key = -key
     translated = ''
 
-    for sybol in message:
+    for symbol in message:
         symbolIndex = SYMBOLS.find(symbol)
-        if symbolIndex == -1: # Символ не найден в SYMBOLS
+        if symbolIndex == -1:  # Символ не найден в SYMBOLS
             #  просто добавить этот символ без изменений
             translated += symbol
         else:
@@ -42,15 +46,12 @@ def getTranslatedMessage(made, message, key):
             elif symbolIndex < 0:
                 symbolIndex += len(SYMBOLS)
 
-            translatd += SYMBOLS(symbolIndex)
+            translated += SYMBOLS[symbolIndex]
     return translated
+# запушил
 
 mode = getMode()
 message = getMessage()
 key = getKey()
 print('Преобразованный текст: ')
 print(getTranslatedMessage(mode, message, key))
-
-
-
-
