763d9efa780be04a1878a29f294e19147820c863
Денис-ПК <denisbataev351@gmail.com>
2022-04-30 21:12:08 +0300

    It's NOT WORKING

--- venv/reversegame.py
+++ venv/reversegame.py
@@ -1,11 +1,8 @@
 # "реверси": клон "Отелло"
 import random
 import sys
-
 WIDTH = 8  # игровое поле содержит 8 клеток по ширине
 HEIGHT = 8  # игровое поле содержит 8 клеток по высоте
-
-
 def drawBoard(board):
     # вывести игровое поле, переданное этой функции. Ничего не возвращать
     print('  12345678')
@@ -18,7 +15,6 @@ def drawBoard(board):
     print(' +--------+')
     print('  12345678')
 
-
 def getNewBoard():
     # создать структуру данных нового чистого игрового поля
     board = []
@@ -26,45 +22,42 @@ def getNewBoard():
         board.append([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
     return board
 
-
 def isValidMove(board, tile, xstart, ystart):
     # вернуть False, если ход игрока в клетку с координатами xstart, ystart недопустимый
     # если это допустимый ход, вернуть список клеток, которые присвоили бы игрок если бы сделал туда ход
-    if board[xstart][ystart] != ' ' or not isOnBoard(xstart, ystart):
+    if board[xstart][ystart] != '' or not isOnBoard(xstart, ystart):
         return False
+
     if tile == 'X':
         otherTile = 'O'
     else:
         otherTile = 'X'
 
-        tilesToFlip = []
-
+    tilesToFlip = []
     for xdirection, ydirection in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
         x, y = xstart, ystart
         x += xdirection  # первый шаг в направлении x
         y += ydirection  # первый шаг в направлении y
-        while isOnBoard(x, y) and board[x, y] == otherTile:
+        while isOnBoard(x, y) and board[x][y] == otherTile:
             #  продолжать двигаться в этом направлении  x и y
             x += xdirection
             y += ydirection
             if isOnBoard(x, y) and board[x][y] == tile:
-                #  есть фишки которые можно перевернутью двигаться в обратном направлении до достижения исходной
-                #  клетки, отмечая все фишки на этом пути
+                #  есть фишки которые можно перевернутью двигаться в обратном направлении до достижения исходной клетки, отмечая все фишки на этом пути
                 while True:
                     x -= xdirection
                     y -= ydirection
                     if x == xstart and y == ystart:
                         break
                     tilesToFlip.append([x, y])
+
     if len(tilesToFlip) == 0:  # если ни одна из фишек не перевернулась это недопустимый ход
         return False
     return tilesToFlip
 
-
 def isOnBoard(x, y):
     # вернуть True если координаты есть на игровом поле
     return x >= 0 and x <= WIDTH - 1 and y >= 0 and y <= HEIGHT - 1
-
 
 def getBoardWithValidMoves(board, tile):
     #  вернуть новое поле с точками, обозначающими допустимые ходы, которые может сделать игрок
@@ -72,8 +65,7 @@ def getBoardWithValidMoves(board, tile):
 
     for x, y in getValidMoves(boardCopy, tile):
         boardCopy[x][y] = '.'
-        return boardCopy
-
+    return boardCopy
 
 def getValidMoves(board, tile):
     #  вернуть список списков с координатами х и у допустимых ходов для данноого игрока на данном игровом поле
@@ -84,12 +76,11 @@ def getValidMoves(board, tile):
                 validMoves.append([x, y])
     return validMoves
 
-
 def getScoreOfBoard(board):
     #  определить количество очков, подсчитав фишки. Вернуть словарь с ключами 'Х' и 'О'
     xscore = 0
     oscore = 0
-    for i in range(WIDTH):
+    for x in range(WIDTH):
         for y in range(HEIGHT):
             if board[x][y] == 'X':
                 xscore += 1
@@ -97,13 +88,12 @@ def getScoreOfBoard(board):
                 oscore += 1
     return {'X': xscore, 'O': oscore}
 
-
 def enterPlayerTile():
     #  позволить игроку ввести выбранную фишку
     #  возвращает список с фишкой игрока в качестве первого элемента и фишкой компьютера в качестве второго
     tile = ''
     while not (tile == 'X' or tile == 'O'):
-        print('вы играете за Х или О?')
+        print('вы играете за X или О?')
         tile = input().upper()
 
     #  первый элемент в списке фишка игрока, второй элемент фишка компа
@@ -112,14 +102,12 @@ def enterPlayerTile():
     else:
         return ['O', 'X']
 
-
 def whoGoesFirst():
     #  случайно выбрать кто ходит первым
     if random.randint(0, 1) == 0:
-        return 'компик'
+        return 'компьютер'
     else:
         return 'человек'
-
 
 def makeMove(board, tile, xstart, ystart):
     #  поместить фишку на игровое поле в позицию xstart, ystart и перевернуть какую либо фишку противкина
@@ -134,21 +122,19 @@ def makeMove(board, tile, xstart, ystart):
         board[x][y] = tile
     return True
 
-
 def getBoardCopy(board):
     #  сделать копию списка board и вернуть ee
     boardCopy = getNewBoard()
 
     for x in range(WIDTH):
-        boardCopy[x][y] = board[x][y]
+        for y in range(HEIGHT):
+            boardCopy[x][y] = board[x][y]
 
     return boardCopy
 
-
 def isOnCorner(x, y):
     #  вернуть True если указанная позиция находиться в одном из 4-х углов
-    return (x == 0 or y == WIDTH - 1) and (y == 0 or y == HEIGHT - 1)
-
+    return (x == 0 or x == WIDTH - 1) and (y == 0 or y == HEIGHT - 1)
 
 def getPlayerMove(board, playerTile):
     #  позволить игроку ввести свой ход
@@ -173,7 +159,6 @@ def getPlayerMove(board, playerTile):
 
     return [x, y]
 
-
 def getComputerMove(board, computerTile):
     #  учитывая данное игровое поле и данную фишку компьютера, определить,
     #  куда сделать ход, и вепнуть этот ход в виде списка[x, y]
@@ -189,3 +174,87 @@ def getComputerMove(board, computerTile):
     bestScore = -1
     for x, y in possibleMoves:
         boardCopy = getBoardCopy(board)
+        makeMove(boardCopy, computerTile, x, y)
+        score = getScoreOfBoard(boardCopy)[computerTile]
+        if score > bestScore:
+            bestMove = [x, y]
+            bestScore = score
+    return bestMove
+
+def printScore(board, playerTile, computerTile):
+    scores = getScoreOfBoard(board)
+    print('Ваш счёт:  %s. Счет компьютера: %s' % (scores[playerTile], scores[computerTile]))
+
+def playGame(playerTile, computerTile):
+    showHints = False
+    turn = whoGoesFirst()
+    print(turn + ' ходит первым')
+
+    #  очистить игровое поле и выставить стартовые фишки
+    board = getNewBoard()
+    board[3][3] = 'X'
+    board[3][4] = 'O'
+    board[4][3] = 'O'
+    board[4][4] = 'X'
+
+    while True:
+        playerValidMoves = getValidMoves(board, playerTile)
+        computerValidMoves = getValidMoves(board, computerTile)
+
+        if playerValidMoves == [] and computerValidMoves == []:
+            return board  # ходов нет ни у когоб закончить игру
+
+        elif turn == 'человек':  # ход человека
+            if playerValidMoves != []:
+                if showHints:
+                    validMovesBoard = getBoardWithValidMoves(board, playerTile)
+                    drawBoard(validMovesBoard)
+                else:
+                    drawBoard(board)
+                printScore(board, playerTile, computerTile)
+
+                move = getPlayerMove(board, playerTile)
+                if move == 'выход':
+                    print('Благодарим за игру!')
+                    sys.exit()  # закончить работу программы
+                elif move == 'подсказка':
+                    showHints = not showHints
+                    continue
+                else:
+                    makeMove(board, playerTile, move[0], move[1])
+            turn = 'компьютер'
+
+        elif turn == 'компьютер':  # ход компьютера
+            if computerValidMoves != []:
+                drawBoard(board)
+                printScore(board, playerTile, computerTile)
+
+                input('Нажмите клавишу Enter для просмотра хода компьютера')
+                move = getComputerMove(board, computerTile)
+                makeMove(board, computerTile, move[0], move[1])
+            turn = 'человек'
+
+
+
+print('ПРИВЕТ В ИГРЕ РЕВЕРСИ!!!')
+
+playerTile, computerTile = enterPlayerTile()
+
+while True:
+    finalBoard = playGame(playerTile, computerTile)
+
+    #  отобразить итоговый счёт
+    drawBoard(finalBoard)
+    scores = getScoreOfBoard(finalBoard)
+    print('X набрал %s очков. O набрал %s очков.' % (scores['X'], scores['O']))
+    if scores[playerTile] > scores[computerTile]:
+        print('Ай молодца, ты выиграл человечишка, обогнал меня на %s очков' % (scores[playerTile] - scores[computerTile] ))
+    elif scores[playerTile] < scores[computerTile]:
+        print('ты продул глупый червяк! ИИ тебя победил как и в Терминаторе всех! он тебя обошёл на %s очков' % (scores[computerTile] - scores[playerTile]))
+    else:
+        print('Да ну на, ничья!')
+
+    print('хочется ещё раз продуть?(да или нет)')
+    if not input().lower().startswith('д'):
+        break
+
