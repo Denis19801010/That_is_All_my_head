c9959e9288d5c5aef45aa7f2a075cb603de4ca87
Денис-ПК <denisbataev351@gmail.com>
2022-04-30 05:28:59 +0300

    add code

--- venv/reversegame.py
+++ venv/reversegame.py
@@ -18,6 +18,7 @@ def drawBoard(board):
     print(' +--------+')
     print('  12345678')
 
+
 def getNewBoard():
     # создать структуру данных нового чистого игрового поля
     board = []
@@ -25,6 +26,44 @@ def getNewBoard():
         board.append([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
     return board
 
+
 def isValidMove(board, tile, xstart, ystart):
+    # вернуть False, если ход игрока в клетку с координатами xstart, ystart недопустимый
+    # если это допустимый ход, вернуть список клеток, которые присвоили бы игрок если бы сделал туда ход
+    if board[xstart][ystart] != ' ' or not isOnBoard(xstart, ystart):
+        return False
+    if tile == 'X':
+        otherTile = 'O'
+    else:
+        otherTile = 'X'
 
+        tilesToFlip = []
 
+    for xdirection, ydirection in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
+        x, y = xstart, ystart
+        x += xdirection  # первый шаг в направлении x
+        y += ydirection  # первый шаг в направлении y
+        while isOnBoard(x, y) and board[x, y] == otherTile:
+            #  продолжать двигаться в этом направлении  x и y
+            x += xdirection
+            y += ydirection
+            if isOnBoard(x, y) and board[x][y] == tile:
+                #  есть фишки которые можно перевернутью двигаться в обратном направлении до достижения исходной
+                #  клетки, отмечая все фишки на этом пути
+                while True:
+                    x -= xdirection
+                    y -= ydirection
+                    if x == xstart and y == ystart:
+                        break
+                    tilesToFlip.append([x, y])
+    if len(tilesToFlip) == 0:  # если ни одна из фишек не перевернулась это недопустимый ход
+        return False
+    return tilesToFlip
+
+def isOnBoard(x, y):
+    # вернуть True если координаты есть на игровом поле
+    return x >= 0 and x <= WIDTH -1 and y >= 0 and y <= HEIGHT -1
+
+def getBoardWithValidMoves(board, tile):
+    #  вернуть новое поле с точками, обозначающими допустимые ходы, которые может сделать игрок
+    boardCopy = getBoardCopy(Copy)
