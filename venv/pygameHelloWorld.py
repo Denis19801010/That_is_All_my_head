d651411c3b4527fea6a89b2b1a26ad0743c648f2
Денис-ПК <denisbataev351@gmail.com>
2022-04-30 21:52:35 +0300

    master

---
+++ venv/pygameHelloWorld.py
@@ -0,0 +1,63 @@
+import pygame
+import sys
+from pygame.locals import *
+
+# настройка pygame
+pygame.init()
+# настройка окна
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Привет МИр!')
+
+# назначение цветов
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# назначение шрифтов
+basicFont = pygame.font.SysFont(None, 48)
+
+# настройка текста
+text = basicFont.render('Привет Мир!', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
+
+#  нанесение на поверхность белого фона
+windowSurface.fill(WHITE)
+
+# нанесение на поверхность зеленого многоугольника
+pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+
+# нанесение на поверхность синих линий
+pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
+pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
+pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
+
+# нанесение на поверхность синего круга
+pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
+
+# нанесени ена поверхность красного эллипла
+pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
+
+# нанесени ена поверхность фонового прямоугольника для текста
+pygame.draw.rect(windowSurface, RED, (textRect.left-20, textRect.top-20, textRect.width+40, textRect.height+40))
+
+# получение массива пикселов поверхности
+pixArray = pygame.PixelArray(windowSurface)
+pixArray[480][380] = BLACK
+del pixArray
+
+# нанесение текста на поверхность
+windowSurface.blit(text, textRect)
+
+# отображение окна на экране
+pygame.display.update()
+
+# запуск игрового цикла
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
