eda78c234f222223f9e6077da7b3f10c5776f8c5
Денис-ПК <denisbataev351@gmail.com>
2022-05-01 20:08:41 +0300

    done

--- venv/collisionDetection.py
+++ venv/collisionDetection.py
@@ -18,7 +18,7 @@ WHITE = (255, 255, 255)
 
 # создание структур данных игрока и "еды"
 foodCounter = 0
-NEWFOOD = 40
+NEWFOOD = 40    # скорость добавления еды
 FOODSIZE = 20
 player = pygame.Rect(300, 100, 50, 50)
 foods = []
@@ -41,9 +41,9 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == KEYDOWN:
+        if event.type == KEYDOWN:  # когда игрок нажимает клавишу.
             # изменение параметров клавиатуры
-            if event.key == K_LEFT or event.key == K_a:
+            if event.key == K_LEFT or event.key == K_a:  # K_a, K_d, K_w, K_s  это кнопки перемещения  w a s d
                 moveRight = False
                 moveLeft = True
             if event.key == K_RIGHT or event.key == K_d:
@@ -55,7 +55,7 @@ while True:
             if event.key == K_DOWN or event.key == K_s:
                 moveUp = False
                 moveDown = True
-        if event.type == KEYUP:
+        if event.type == KEYUP:  # когда игрок отпускает клавишу.
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
@@ -71,17 +71,40 @@ while True:
                 player.top = random.randint(0, WINDOWHEIGHT - player.height)
                 player.left = random.randint(0, WINDOWWIDTH - player.width)
 
-        if event.type == MOUSEBUTTONUP:
+        if event.type == MOUSEBUTTONUP:  # когда игрок перемещает мышь по окну.
             foods.append(pygame.Rect(event.pos[0], event.pos[1], FOODSIZE, FOODSIZE))
 
     foodCounter += 1
     if foodCounter >= NEWFOOD:
         # добавление новой "еды"
         foodCounter = 0
-        foods.append(pygame.Rect(random.randint((0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE)))
+        foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
 
    # создание поверхности белого фона
-    windowSurfase.fill(WHITE)
+    windowSurface.fill(WHITE)
 
    # перемещение игрока
-   if moveDown and player.bottom < WINDOWHEIGHT
+    if moveDown and player.bottom < WINDOWHEIGHT:
+        player.top += MOVESPEED
+    if moveUp and player.top > 0:
+        player.top -= MOVESPEED
+    if moveLeft and player.left > 0:
+        player.left -= MOVESPEED
+    if moveRight and player.right < WINDOWWIDTH:
+        player.right += MOVESPEED
+
+    # отображение игрока на поверхности
+    pygame.draw.rect(windowSurface, BLACK, player)
+
+    # проверка не пересёкся игрок с каким либо блоком "еды"
+    for food in foods[:]:
+        if player.colliderect(food):
+            foods.remove(food)
+
+    # отображение еды
+    for i in range(len(foods)):
+        pygame.draw.rect(windowSurface, GREEN, foods[i])
+
+    # вывод окна на экран
+    pygame.display.update()
+    mainClock.tick(40 )
