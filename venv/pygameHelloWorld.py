54f80eb4f7d6d6c3d14031d7cc704a181b13d0cd
Денис-ПК <denisbataev351@gmail.com>
2022-05-01 16:40:09 +0300

    master

--- venv/pygameHelloWorld.py
+++ venv/pygameHelloWorld.py
@@ -19,7 +19,7 @@ BLUE = (0, 0, 255)
 basicFont = pygame.font.SysFont(None, 48)
 
 # настройка текста
-text = basicFont.render('Привет Мир!', True, WHITE, BLUE)
+text = basicFont.render('Привет мир', True, WHITE, BLUE)
 textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx
 textRect.centery = windowSurface.get_rect().centery
@@ -33,7 +33,7 @@ pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56
 # нанесение на поверхность синих линий
 pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
 pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
-pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
+pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 8)
 
 # нанесение на поверхность синего круга
 pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
@@ -44,7 +44,7 @@ pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
 # нанесени ена поверхность фонового прямоугольника для текста
 pygame.draw.rect(windowSurface, RED, (textRect.left-20, textRect.top-20, textRect.width+40, textRect.height+40))
 
-# получение массива пикселов поверхности
+# получение массива пикселов поверхности( в сааааамом низу справа один пиксель)
 pixArray = pygame.PixelArray(windowSurface)
 pixArray[480][380] = BLACK
 del pixArray
